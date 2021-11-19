3bcf4b4eb3a270b5013a1978d62f7bce2a0f53fc
Garcia <jimmygarcian@outlook.fr>
2021-11-19 15:59:06 +0100

    Jfiglet

---
+++ src/main/java/dev/appJfiglet.java
@@ -0,0 +1,12 @@
+package dev;
+import com.github.lalyos.jfiglet.FigletFont;
+
+public class appJfiglet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String asciiArt = FigletFont.convertOneLine("App");
+		System.out.println(asciiArt);
+	}
+
+}
